8a4d241cbe39e089efc8018c5be45721dd3cee5b
Ramin Mammadzada <raminmammadzadaiu@gmail.com>
2022-08-21 19:53:58 +0300

    Add unit tests for Price Repository.

--- pricing-service/src/test/java/com/udacity/pricing/web/PriceRepositoryTest.java
+++ pricing-service/src/test/java/com/udacity/pricing/web/PriceRepositoryTest.java
@@ -1,4 +1,43 @@
 package com.udacity.pricing.web;
 
-public class PriceRepo {
+import com.udacity.pricing.domain.price.Price;
+import com.udacity.pricing.domain.price.PriceRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Optional;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PriceRepositoryTest {
+    @Autowired
+    PriceRepository priceRepository;
+
+    @Test
+    public void getAllPrices() {
+        List<Price> prices = (List<Price>) priceRepository.findAll();
+        assertEquals(6, prices.size());
+    }
+
+    @Test
+    public void getPrice() {
+        Optional<Price> optionalPrice = priceRepository.findById(2L);
+        assertTrue(optionalPrice.isPresent());
+    }
+
+
 }
